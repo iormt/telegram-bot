f938a2bbac865bc9d2043fd78ae3bc5011e71857
Ramiro Travieso <ragtre@gmail.com>
2024-08-16 05:38:50 -0300

    moved counter_data.json to data folder and refactored

--- constants_module/constants.py
+++ constants_module/constants.py
@@ -3,3 +3,4 @@ from typing import Final
 
 OPEN_AI_MODEL: Final = 'gpt-4o-mini'
 BOT_USERNAME: Final = '@delto_prueba_tecnica_bot'
+COUNTER_DATA_PATH: Final = 'data\counter_data.json'
--- counter_module/counter.py
+++ counter_module/counter.py
@@ -1,15 +1,17 @@
 import json
 import os
-
+from constants_module import constants
 
 
 counter_data = {}
 
+
+
 def save_counter_data():
-    with open('counter_data.json', 'w') as file:
+    with open(constants.COUNTER_DATA_PATH, 'w') as file:
         json.dump(counter_data, file)
 
 
-if os.path.exists('counter_data.json'):
-    with open('counter_data.json', 'r') as file:
+if os.path.exists(constants.COUNTER_DATA_PATH):
+    with open(constants.COUNTER_DATA_PATH, 'r') as file:
         counter_data = json.load(file)
